64aa71280da06dea22968775c3a5b9f722f68b9d
Shawn Hsu <goodshawn12@gmail.com>
2014-11-21 17:52:36 -0800

    worked version 1.2
    fixed component selection, tested on 64 channel dataset.

--- run_visORICA.m
+++ run_visORICA.m
@@ -1,11 +1,25 @@
 %% Run visGUI
 
 % load channel locations
-load vis/chanlocs14
+% load vis/chanlocs14
 
 % load calibration data
-calibData = exp_eval_optimized(io_loadset('data/EmotivTrain_EyeClose_icainfo.set', ...
+% calibData = exp_eval_optimized(io_loadset('data/EmotivTrain_EyeClose_icainfo.set', ...
+%     'markerchannel',{'remove_eventchns',false}));
+
+% load 61-ch Flanker Task dataset
+% calibData = exp_eval_optimized(io_loadset('D:\Matlab Coding\OnlineICA\BCILAB\userdata\Flanker_SH_cleaned_test_icainfo.set', ...
+%     'markerchannel',{'remove_eventchns',false}));
+
+% load 64-ch simulated dataset
+calibData = exp_eval_optimized(io_loadset('D:\Matlab Coding\OnlineICA\BCILAB\userdata\Colin27_Biosemi_1010_0_norot.set', ...
     'markerchannel',{'remove_eventchns',false}));
+
+calibData.event = [];
+chanlocs = calibData.chanlocs;
+
+% [filename, pathname] = uigetfile('*.set');
+
 % select data
 % calibData = set_selinterval(calibData, opts.calibEpoch);
 % calibData = pop_loadset('data/EmotivTrain_EyeClose_icainfo.set');
