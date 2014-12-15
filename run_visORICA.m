cdfc46a8d7a00fcafe82f67a3e788e518bcfe967
goodshawn12 <goodshawn12@gmail.com>
2014-12-15 14:44:18 -0800

    updated run_visORICA

--- run_visORICA.m
+++ run_visORICA.m
@@ -4,16 +4,16 @@
 % load vis/chanlocs14
 
 % load calibration data
-% calibData = exp_eval_optimized(io_loadset('data/EmotivTrain_EyeClose_icainfo.set', ...
-%     'markerchannel',{'remove_eventchns',false}));
+calibData = exp_eval_optimized(io_loadset('data/EmotivTrain_EyeClose_icainfo.set', ...
+    'markerchannel',{'remove_eventchns',false}));
 
 % load 61-ch Flanker Task dataset
 % calibData = exp_eval_optimized(io_loadset('D:\Matlab Coding\OnlineICA\BCILAB\userdata\Flanker_SH_cleaned_test_icainfo.set', ...
 %     'markerchannel',{'remove_eventchns',false}));
 
 % load 64-ch simulated dataset
-calibData = exp_eval_optimized(io_loadset('D:\Matlab Coding\OnlineICA\BCILAB\userdata\Colin27_Biosemi_1010_0_norot.set', ...
-    'markerchannel',{'remove_eventchns',false}));
+% calibData = exp_eval_optimized(io_loadset('D:\Matlab Coding\OnlineICA\BCILAB\userdata\Colin27_Biosemi_1010_0_norot.set', ...
+%     'markerchannel',{'remove_eventchns',false}));
 
 calibData.event = [];
 chanlocs = calibData.chanlocs;
