5047b454ef7d2d11301b5a18f40a7e325e30a4a7
LucaPT <lucapton@yahoo.com>
2015-08-12 19:33:02 -0700

    cleaned test_script. plan to incorporate data playback into REST

--- test_script.m
+++ test_script.m
@@ -1,23 +1,28 @@
-%% Run REST
+%% Test REST
+
 %% refresh workspace
 % clear all will break bcilab and require it to restart as it uses global
 % variables 
 clear
 close all
 delete(timerfind)
+
 %% set path and 
 addpath(genpath(['.' filesep]));
+% start bcilab or add it to the path as well
 
-playback = 1;
+%% load data and create opts structure
+% point to headModel
+opts.headModel = ['data' filesep 'head_models' filesep 'emotivHeadModel_file'];
 
 % load calibration data
 data_location = ['data' filesep 'Demo_EmotivEPOC_EyeClose.set'];
-opts.headModel = ['data' filesep 'head_models' filesep 'emotivHeadModel_file'];
 opts.calibration_data = exp_eval_optimized(io_loadset(data_location, ...
         'markerchannel',{'remove_eventchns',false}));
+opts.calibration_data = pop_select(opts.calibration_data,'time',[0 10]);
 
-opts.calibration_data = pop_select(opts.calibration_data,'time',[0 1]);
-
+%% create LSL stream for online EEG data playback
+playback = 1;
 if playback
     if exist('playbackStream','var')
         try %#ok<TRYNC>
@@ -29,10 +34,11 @@ if playback
     playbackStream = play_eegset_lsl(EEG,'REST_test_data','REST_test_markers',[],true);
 end
 
-% start REST
+%% start REST
 REST(opts)
 
-% if playback
-%     stop(playbackStream)
-%     delete(playbackStream)
-% end
+%% delete playback stream
+if playback
+    stop(playbackStream)
+    delete(playbackStream)
+end
