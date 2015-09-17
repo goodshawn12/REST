115c9b85be90d6afd2113d3f2a05d27900bfd8bc
Shawn Hsu <goodshawn12@gmail.com>
2015-09-16 17:19:30 -0700

    version for Emotiv EPOC

--- test_script.m
+++ test_script.m
@@ -12,24 +12,21 @@ addpath(genpath(['.' filesep]));
 % start bcilab or add it to the path as well
 
 %% define opts structure
+% whether to customize pipeline 
 opts.customize_pipeline = true;
-opts.config = 'Config_ORICA_quick20';
 
-% load channel location file
-load(['data' filesep 'chanlocs' filesep 'Quick20.mat']);
-opts.chanlocs = chanlocs;
+% (optional) define config file name
+opts.config = 'Config_ORICA_EmotivEPOC';
 
 % point to headModel
-% opts.headModel = ['data' filesep 'head_models' filesep 'emotivHeadModel_file'];
-opts.headModel = ['data' filesep 'head_models' filesep 'quick20HeadModel'];
+opts.headModel = ['data' filesep 'head_models' filesep 'emotivHeadModel_file'];
 
 % (optional) path to calibration data and select time window
-% opts.calibration_data = ['data' filesep 'Demo_EmotivEPOC_EyeClose.set'];
-opts.calibration_data = ['data' filesep 'quick20_calib_EyeOpen.set'];
+opts.calibration_data = ['data' filesep 'Demo_EmotivEPOC_EyeClose.set'];
 opts.calibration_window = [0,60]; % sec
 
 % use playback data
-opts.playback = 0;
+opts.playback = 1;
 
 %% start REST
 REST(opts)
